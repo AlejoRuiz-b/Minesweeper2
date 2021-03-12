6d0444ecf538e5c363c0d5e62732ebf05582535b
la_ruizbareno <alejoruiz315@gmail.com>
2021-03-12 15:46:37 +0100

    Testing

--- game.py
+++ game.py
@@ -1,12 +1,1 @@
-import pyHook
-import pythoncom
-
-def onclick(event):
-    print event.Position
-    return True
-
-hm = pyHook.HookManager()
-hm.SubscribeMouseAllButtonsDown(onclick)
-hm.HookMouse()
-pythoncom.PumpMessages()
-hm.UnhookMouse()
+sdsds
