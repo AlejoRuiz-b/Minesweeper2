eb8a5819d17faee0e4656cd66b6a84736ff9b4c5
la_ruizbareno <alejoruiz315@gmail.com>
2021-03-17 09:40:48 +0100

    test creation

--- game.py
+++ game.py
@@ -1,16 +1,18 @@
 from board import Board
 
+
 class Game(object):
     def __int__(self):
-        self.board = Board(10,10)
+        self.board = Board(10, 10)
+        self.board.countNeighbords()
+        self.board.printMatrix()
+
 
     def test(self):
-       # self.board.countNeighbords()
-        self.board.printMatrix()
-        self.board.printmatrix2()
-
-
+        self.board.countNeighbords()
+        """self.board.printMatrix()
+        self.board.printmatrix2()"""
 
 
 game = Game()
-game.board.printMatrix()
+#game.board.printMatrix()
